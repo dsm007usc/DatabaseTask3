e46a8f1926584924c3420039f5b776f3be979c77
dsm007usc <dsm007@student.usc.edu.au>
2023-10-22 13:03:53 +1000

    main

--- Task 3 - Pizza App.py
+++ Task 3 - Pizza App.py
@@ -2,8 +2,6 @@
 
 import pyodbc
 
-import math
-
 from pymongo import MongoClient
 import json
 # Get these values from the Azure portal page for your cosmos db account. Change the details to fit your database and collection
@@ -62,42 +60,46 @@ def get_customer_details(connection, customer_id):
 
 def create_Delivery_Doc(connection, order_id, customer_id):
     """Shows all the records for a given table."""
-    itemsList = {}
+    dilivery_doc = {}
     customer_list = get_customer_details(connection,customer_id)
     order_list = get_order_items_sql(connection, order_id)
-    itemsList.update({'customer_name' : customer_list['first_Name'] + " " + customer_list['last_Name'], 
+    order_Total = total_Cost(order_list)
+    driver = allocate_Driver(customer_list['post_code'])
+    dilivery_doc.update({'customer_name' : customer_list['first_Name'] + " " + customer_list['last_Name'], 
                       'customer_address' : customer_list['address'],
                       'customer_postcode' : customer_list['post_code'],
                       'Pizzas' : order_list,
-                      'Total_Cost' : total_Cost(order_list),
-                      'Driver' : allocate_Driver(customer_list['post_code'])
+                      'Total_Cost' : order_Total,
+                      'Driver' : driver,
+                      'Comission' : round((driver['commission_Rate']/100) * order_Total, 2)
                       })
-    return(itemsList)
+    return(dilivery_doc)
 
 def allocate_Driver(post_code):
     order_delivery_zone = roundDown(post_code)
     driver = my_drivers.find_one({"delivery_Code": order_delivery_zone}, {'_id':0})
     return driver
 
+def roundDown(n):
+    n = n[:-2] + '00'
+    return int(n)
+
 def total_Cost(order_list):
     total = 0
     for order in order_list:
         total += order['Quantity'] * order['Price_Each']
-    return total
+    return round(total,2)
 
-def create_Cooking_Doc(connection, order_id):
+def create_Cooking_Doc(connection, order_id, customer_id):
     """Shows all the records for a given table."""
-    cursor = connection.cursor()
-    cursor.execute("SELECT * \n"
-                   + "FROM [pizza].[order_items] as p\n"
-                   + "where [p].[order_id] = '" + str(order_id) + "' ")
-    rows = cursor.fetchall()
-    itemsList = []
-    for row in rows:
-       itemsList.append({'Pizza_Name' : row[2]  , 'Quantity': row[3] , 'Price_Each' :  float(row[4])})
-    return(itemsList)
-    
-
+    cooking_doc = {}
+    customer_list = get_customer_details(connection,customer_id)
+    order_list = get_order_items_sql(connection, order_id)
+    cooking_doc.update({'customer_name' : customer_list['first_Name'] + " " + customer_list['last_Name'], 
+                      'Pizzas' : order_list,
+                      'Total_Cost' : total_Cost(order_list),
+                      })
+    return(cooking_doc) 
 
 def show_records_for_day(connection, day):
     """Shows all the records for a given table."""
@@ -114,32 +116,50 @@ def show_records_for_day(connection, day):
                 "customer_Info" : get_customer_details(connection,row[1]),
                 "order_date" : str(row[2]),
                 "items" : get_order_items_sql(connection,row[0]),
-                "delivery_Docket" : create_Delivery_Doc(connection,row[0],row[1])
-                
+                "delivery_Docket" : create_Delivery_Doc(connection,row[0],row[1]),
+                "cooking_Docket" : create_Cooking_Doc(connection,row[0],row[1])
             }   
         )
         print(get_customer_details(connection,row[1])['first_Name'])
+    todays_Date = str(row[2])
+    daily_Summary(todays_Date)
 
-"""def insert_record(connection, mytable, myrecord):
-    Inserts a given record into a given table.
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO "+mytable+" VALUES "+ str(myrecord)+";")
-    connection.commit()"""
+def daily_Summary(date):
+    """total driver commission for the day"""
+    total_orders = 0
+    daily_Items = []
+    days_drivers = {}
+    
+    for doc in my_orders.find({"order_date" : date}):
+        total_orders += 1
+        daily_Items += doc["items"]
+        
+        if days_drivers.get(doc["delivery_Docket"]["Driver"]['driver_Name']) == None:
+            days_drivers.update({doc["delivery_Docket"]["Driver"]['driver_Name']: doc["delivery_Docket"]["Comission"]})
+        else:
+            days_drivers[doc["delivery_Docket"]["Driver"]['driver_Name']] += doc["delivery_Docket"]["Comission"]
+            
+    total_sales = total_Cost(daily_Items)
+    print("total orders are " + str(total_orders))
+    print("total sales is " + str(total_sales))
+    print(days_drivers)
+    print("favaroute pizzas are " + str(find_Fav_Pizza(daily_Items)))
+    return
 
-def general_query(connection):
-    """Does a general quesry on the database. Be careful with sql injection!"""
-    sql = input("Enter your query: ")
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-def roundDown(n):
-    n = n[:-2] + '00'
-    return int(n)
+def find_Fav_Pizza(daily_Items):
+    favourates_List = []
+    pizza_List = {"Hawaiian" : 0, "Margherita" : 0, "Meatlovers" : 0 , "Pepperoni" : 0 , "Supreme" : 0 , "Vegetarian" : 0}
+    for items in daily_Items:
+        pizza_name = items['Pizza_Name']
+        pizza_List[pizza_name]= pizza_List.get(pizza_name) + items['Quantity']
+    maximum = max(pizza_List.values())
+    for i in pizza_List:
+        if pizza_List.get(i) == maximum:
+            favourates_List.append(i)
+    return favourates_List
 
 connection = connect_to_azure_sql_database()
+my_orders.drop()
 show_records_for_day(connection, 2)
 
 connection.close()
