1fe4e5a50110917100bf6a4bc89d1048cf29b9d6
Dylan Mark <dsm007@student.usc.edu.au>
2023-10-25 15:28:07 +1000

    main

--- Querys/CREATE VIEW.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW [order_date_to_days] AS
-SELECT [order_date],
-ROW_NUMBER() OVER(ORDER BY(SELECT 1)) [order_day]
-FROM [pizza].[orders]
-GROUP by [order_date]
--- Querys/select day 1.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM [pizza].[orders] as p
-LEFT OUTER JOIN [dbo].[order_date_to_days] as v On p.order_date = v.order_date
-where [v].[order_day] = '1'
